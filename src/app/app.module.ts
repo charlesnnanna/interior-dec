07b7291ac6ee1b6b38ac78ae216d5f2d2f0bdc8d
unknown <Metaphor1667>
2021-12-06 05:16:41 -0800

    Added header section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header.component';
+import { HeroComponent } from './home/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent
+
   ],
   imports: [
     BrowserModule
