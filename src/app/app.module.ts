6cde0960be6743caef4bbf321669b59ec5bed238
unknown <Metaphor1667>
2021-12-30 10:34:54 -0800

    Pushed header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './home/header.component';
@@ -13,7 +14,8 @@ import { HeroComponent } from './home/hero.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/header.component.ts
+++ src/app/home/header.component.ts
@@ -1,10 +1,35 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        opacity : "1",
+        zIndex : "1"
+      })),
+      state('closed', style({
+        opacity : "0",
+        zIndex : "-1"
+      })),
+      transition('open => closed', [
+        animate('0.2s ease-out')
+      ]),
+      transition('closed => open', [
+        animate('0.2s ease-in')
+      ]),
+    ]),
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   title = 'interior-dec';
+  showNav: boolean = false;
+  toggleNav(): void{
+    this.showNav = !this.showNav;
+    console.log(this.showNav);
+  }
 }
